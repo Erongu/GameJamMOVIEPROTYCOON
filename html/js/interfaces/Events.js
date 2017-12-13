759330666f50fbb92cc8f2652f1a3e48fdeb2b5b
Alpaglace <alpaoff@gmail.com>
2017-12-13 12:35:18 +0100

    Add Animation

--- html/js/interfaces/Events.js
+++ html/js/interfaces/Events.js
@@ -1,5 +1,8 @@
-$($(function () {
+$(function () {
     $("#connect_login").on('click', function () {
-        $(this).css("display","none");
+        $("#login_popup").addClass("fadeOut animated").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $(this).css("display","none");
+        });
+        //$("#login_popup").css("display","none");
     });
 });
