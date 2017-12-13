139d26cafb6ba951f33dd43fa4ff68dd4f08396f
Alpaglace <alpaoff@gmail.com>
2017-12-13 13:08:05 +0100

    tre

--- html/js/interfaces/Events.js
+++ html/js/interfaces/Events.js
@@ -2,7 +2,7 @@ $(function () {
     $("#connect_login").on('click', function () {
         $("#menu_popup").css("display", "block");
         $("#menu_popup").addClass("fadeInDown animated");
-        
+
         $("#login_popup").addClass("fadeOut animated").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $(this).css("display", "none");
             //$("#login_popup").css("display","none");
