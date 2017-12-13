cef9405f0413538892931209e89f084a3336aa08
Erongu <dmeschin@msn.com>
2017-12-13 19:42:09 +0100

    OK

---
+++ BORDEL/sans-titre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SDFG" tilewidth="270" tileheight="460" tilecount="84" columns="6">
+ <image source="../../../../Downloads/Untitled-3.png" width="1764" height="6444"/>
+</tileset>
