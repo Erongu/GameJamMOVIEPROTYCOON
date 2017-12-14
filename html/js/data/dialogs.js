6da5655a03e74b234a8a8a8a39e815602aa5a3bd
Erongu <dmeschin@msn.com>
2017-12-14 16:40:19 +0100

    Texte de bienvenu

--- html/js/data/dialogs.js
+++ html/js/data/dialogs.js
@@ -6,5 +6,15 @@ var Dialogs = [
 	{ // 1
 		title: "Make a new film !",
 		body: "blablabla"
-	}
+	},
+	{ // 2
+		title: "Welcome !",
+		body: "Good day {playername}, and welcome to the movie business ! <br>
+			<br>
+			To be honest, I really don't see where the money for this <br>
+			new technology will come from, but I am happy, you bought <br>
+			my old studio for it.<br>
+			<br>
+			Let's have a look around..."
+	},
 ];
