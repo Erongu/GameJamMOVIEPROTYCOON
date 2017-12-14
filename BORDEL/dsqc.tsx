842f2663b163711533efd125aea02dc8afcf1366
Erongu <dmeschin@msn.com>
2017-12-14 11:37:49 +0100

    test menu joiurnal

---
+++ BORDEL/dsqc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meubles" tilewidth="70" tileheight="128" tilecount="80" columns="16">
+ <image source="sprite_show_by_rbl3d-d78xvjw.png" width="1164" height="725"/>
+</tileset>
