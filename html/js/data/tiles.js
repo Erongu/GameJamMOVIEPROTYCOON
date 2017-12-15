79d095b39bbc81dbd097b50379d006cfb1d50284
NairolfH <florian.catel@viacesi.fr>
2017-12-15 10:29:39 +0100

    Ajout décors

--- html/js/data/tiles.js
+++ html/js/data/tiles.js
@@ -30,5 +30,76 @@ var Tiles = [
 			y: 0
 		}
 	},
+	{ // machine a écrire
+		uid: 3,
+		gfx_id: 22,
+		cell_id: 846,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // chaisse
+		uid: 4,
+		gfx_id: 11,
+		cell_id: 347,
+		reverse: true,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // plante
+		uid: 5,
+		gfx_id: 25,
+		cell_id: 845,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // Spot
+		uid: 6,
+		gfx_id: 13,
+		cell_id: 549,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // table basse
+		uid: 7,
+		gfx_id: 31,
+		cell_id: 1298,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // tapis rouge
+		uid: 8,
+		gfx_id: 26,
+		cell_id: 628,
+		reverse: true,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+{ // plante2
+		uid: 9,
+		gfx_id: 25,
+		cell_id: 623,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+
 
 ];
