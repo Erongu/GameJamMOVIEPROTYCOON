1672654807af59fea6a4bf1ac1e347734c425285
Alpaglace <alpaoff@gmail.com>
2017-12-15 06:17:46 +0100

    a

--- html/js/data/Actors.js
+++ html/js/data/Actors.js
@@ -2,7 +2,7 @@ var Actors = [
 	{ // Actor 0
 		uid: 0,
 		name: "Johnny Derp",
-		gfx_id: 1,
+		gfx_id: 0,
 		age: 54,
 		price: 0,
 		speciality:3
@@ -15,31 +15,28 @@ var Actors = [
         price: 100,
 		speciality:0
 	},
-];
 	{ // Actor 2
 		uid: 2,
 		name: "Edi Gathegi",
-		gfx_id: 1,
+		gfx_id: 2,
 		age: 38,
-        price: 100,
+        price: 1000,
 		speciality:2
 	},
-];
 	{ // Actor 3
 		uid: 3,
 		name: "Kristen Steward",
-		gfx_id: 1,
+		gfx_id: 3,
 		age: 27,
-        price: 100,
+        price: 5000,
 		speciality:1
 	},
-];
 	{ // Actor 4
 		uid: 4,
 		name: "Json Tatane",
-		gfx_id: 1,
+		gfx_id: 4,
 		age: 50,
-        price: 100,
+        price: 10000,
 		speciality:4
-	},
+	}
 ];
--- html/js/data/Genres.js
+++ html/js/data/Genres.js
@@ -2,28 +2,23 @@ var Genre = [
 	{ // Adventure
 		uid: 0,
 		name: "Adventure"
-	}
-];
+	},
 	{ // Comedy
 		uid: 1,
 		name: "Comedy"
-	}
-];
+	},
 	{ // Thriller
 		uid: 2,
 		name: "Thriller"
-	}
-];
+	},
 	{ // History
 		uid: 3,
 		name: "History"
-	}
-];
+	},
 	{ // Action
 		uid: 4,
 		name: "Action"
-	}
-];
+	},
 	{ // Documentaire
 		uid: 5,
 		name: "Documentaire"
