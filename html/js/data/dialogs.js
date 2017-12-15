e4046417d0c314672d053457135750e27933a469
Erongu <dmeschin@msn.com>
2017-12-15 06:49:03 +0100

    Super

--- html/js/data/dialogs.js
+++ html/js/data/dialogs.js
@@ -3,7 +3,7 @@ var Dialogs = [
 		title: "Welcome !",
 		body: "Welcome {PLAYER_NAME} to the movie business ! <br>\n" +
         "\t\t\t<br>\n" +
-        "\t\t\tTo be honest, I don't why you invested that much for this studio. <br>\n" +
+        "\t\t\tTo be honest, I don't know why you invested that much for this studio. <br>\n" +
         "\t\t\tI hope that the production of your movies will be worth for you ! <br>\n" +
         "\t\t\t<br>\n" +
         "\t\t\tLet's have a look around..."
