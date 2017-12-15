fba9a5e4189fc4536aa59a28e54565125fa9d7df
NairolfH <florian.catel@viacesi.fr>
2017-12-15 10:56:33 +0100

    ajout décors 2

--- html/js/data/tiles.js
+++ html/js/data/tiles.js
@@ -33,7 +33,7 @@ var Tiles = [
 	{ // machine a écrire
 		uid: 3,
 		gfx_id: 22,
-		cell_id: 846,
+		cell_id: 790,
 		reverse: false,
 		offset:{
 			x: 0,
@@ -83,7 +83,7 @@ var Tiles = [
 	{ // tapis rouge
 		uid: 8,
 		gfx_id: 26,
-		cell_id: 628,
+		cell_id: 687,
 		reverse: true,
 		offset:{
 			x: 0,
@@ -97,9 +97,109 @@ var Tiles = [
 		reverse: false,
 		offset:{
 			x: 0,
+			y: 15
+		}
+	},
+	{ // spot
+		uid: 10,
+		gfx_id: 13,
+		cell_id: 796,
+		reverse: true,
+		offset:{
+			x: 0,
 			y: 0
 		}
 	},
+	{ // spot2
+		uid: 11,
+		gfx_id: 13,
+		cell_id: 965,
+		reverse: true,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // table
+		uid: 12,
+		gfx_id: 27,
+		cell_id: 1408,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // table
+		uid: 13,
+		gfx_id: 27,
+		cell_id: 1634,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // statue
+		uid: 14,
+		gfx_id: 21,
+		cell_id: 1183,
+		reverse: true,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // rubn
+		uid: 15,
+		gfx_id: 17,
+		cell_id: 1071,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // fond vert
+		uid: 16,
+		gfx_id: 8,
+		cell_id: 854,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+	},
+	{ // chaisse
+		uid: 17,
+		gfx_id: 11,
+		cell_id: 1242,
+		reverse: true,
+		offset:{
+			x: 0,
+			y: 0
+		}
+		},
+		{ // chaisse
+		uid: 18,
+		gfx_id: 11,
+		cell_id: 1131,
+		reverse: true,
+		offset:{
+			x: 0,
+			y: 0
+		}
+		},
+		{ // chaisse
+		uid: 19,
+		gfx_id: 11,
+		cell_id: 1187,
+		reverse: false,
+		offset:{
+			x: 0,
+			y: 0
+		}
+		},
 
 
 ];
