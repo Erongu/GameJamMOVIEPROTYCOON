a0a2b10b34730d2a33c05b0d87d46744cd5552a5
Erongu <dmeschin@msn.com>
2017-12-15 04:39:18 +0100

    SYMBIOSE !

--- html/js/data/Actors.js
+++ html/js/data/Actors.js
@@ -1,18 +1,45 @@
 var Actors = [
-	{ // Actor 1
+	{ // Actor 0
 		uid: 0,
-		name: "Bruce Lee",
+		name: "Johnny Derp",
 		gfx_id: 1,
-		age: 24,
+		age: 54,
 		price: 0,
-		speciality:0 
+		speciality:3
 	},
-	{ // Actor 2
+	{ // Actor 1
 		uid: 1,
-		name: "Bruce Leepiscine",
+		name: "Brush Lee",
 		gfx_id: 1,
-		age: 25,
+		age: 28,
         price: 100,
-		speciality:0 
+		speciality:0
+	},
+];
+	{ // Actor 2
+		uid: 2,
+		name: "Edi Gathegi",
+		gfx_id: 1,
+		age: 38,
+        price: 100,
+		speciality:2
+	},
+];
+	{ // Actor 3
+		uid: 3,
+		name: "Kristen Steward",
+		gfx_id: 1,
+		age: 27,
+        price: 100,
+		speciality:1
+	},
+];
+	{ // Actor 4
+		uid: 4,
+		name: "Json Tatane",
+		gfx_id: 1,
+		age: 50,
+        price: 100,
+		speciality:4
 	},
 ];
